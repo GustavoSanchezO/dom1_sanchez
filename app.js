a0bdf0e03f56c2b3982776cda6b7e01944d2c166
Gustavo Sanchez O <gustavo_3141240147@utd.edu.mx>
2026-06-03 11:48:40 -0600

    cache de errores

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ btnGenerar.addEventListener('click', () => {
 
     // Removemos la clase 'hidden' para que el contenedor sea visible en la pantalla
     resultadoContenedor.classList.remove('hidden');
-    
+    console.log("la conversion del correo se realizo con exito ")
+
     // Opcional: Cambiamos el estilo dinámicamente mediante el DOM para dar feedback visual
     tokenOutput.style.backgroundColor = "#e8f8f5"; 
     tokenOutput.style.borderLeftColor = "#2ecc71"; // Cambia a verde al completarse
